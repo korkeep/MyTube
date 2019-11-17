1695952fa1690a684607ddcae08b993995a59329
korkeep <korkeep@khu.ac.kr>
2019-11-17 20:53:38 +0900

    로딩화면 구성완료

---
+++ SW Online Lecture APP/app/src/main/java/com/example/swonlinelectureapp/IntroActivity.java
@@ -0,0 +1,23 @@
+package com.example.swonlinelectureapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class IntroActivity extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(3000); // 3초 인트로 화면 보여주기
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // next Activity 기재.
+        startActivity(new Intent(this,LoginActivity.class));
+        finish();
+    }
+}

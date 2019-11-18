8eb151a025c258789cfa9e2dc8c89bc2d12567f2
korkeep <korkeep@khu.ac.kr>
2019-11-19 00:45:31 +0900

    MainActivity 하단 네비게이션바 구성완료

--- SW Online Lecture APP/app/src/main/java/com/example/swonlinelectureapp/MainActivity.java
+++ SW Online Lecture APP/app/src/main/java/com/example/swonlinelectureapp/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.swonlinelectureapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -9,6 +13,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //중간 Frame 구성
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentHome fragmentHome = new FragmentHome();
+    private FragmentRecommend fragmentRecommend = new FragmentRecommend();
+    private FragmentStore fragmentStore = new FragmentStore();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +27,17 @@ public class MainActivity extends AppCompatActivity {
         //액션바 글자 없애고, 뒤로가기 버튼 생성
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("");
+
+        //중간 내용 영역 바
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+
+        //하단 메뉴 구성 바
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
     }
 
+    //상단 부분
     //검색버튼 클릭했을때 동작
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,5 +73,28 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    //하단 메뉴 부분
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.homeItem:
+                    transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+                    break;
+                case R.id.recommendItem:
+                    transaction.replace(R.id.frameLayout, fragmentRecommend).commitAllowingStateLoss();
+                    break;
+                case R.id.storeItem:
+                    transaction.replace(R.id.frameLayout, fragmentStore).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
     }
 }

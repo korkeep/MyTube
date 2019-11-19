792e65b1971754ca570bb16b9fc4a7a15ec125b7
korkeep <korkeep@khu.ac.kr>
2019-11-19 20:47:48 +0900

    Home UI 구성 완료

--- SW Online Lecture APP/app/src/main/java/com/example/swonlinelectureapp/MainActivity.java
+++ SW Online Lecture APP/app/src/main/java/com/example/swonlinelectureapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -24,11 +25,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //액션바 글자 없애고, 뒤로가기 버튼 생성
+        //액션바 글자 없애고, 뒤로가기 생성
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("");
 
-        //중간 내용 영역 바
+        //중간 내용
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
 
@@ -38,11 +39,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //상단 부분
-    //검색버튼 클릭했을때 동작
+    //로고 삽입
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.search_item, menu);
-        /*
+        getMenuInflater().inflate(R.menu.top_bar, menu);
+        /* 검색기능 구현
         MenuItem searchItem = menu.findItem(R.id.action_search);
         searchView = (SearchView) searchItem.getActionView();
         searchView.setQueryHint(getString(R.string.edittext_search_video));
@@ -63,18 +64,23 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    //뒤로가기 클릭했을때의 동작
+    //뒤로가기 동작
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
-        if (id == android.R.id.home) {
-            Toast.makeText(this, "뒤로가기 클릭", Toast.LENGTH_SHORT).show();
-            //뒤로가기 구현해야댕
-            return true;
+        switch(id){
+            case android.R.id.home:
+                //뒤로가기 동작
+                //NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.go_home:
+                //홈으로 이동 동작
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
-
 
     //하단 메뉴 부분
     class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{

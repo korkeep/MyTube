b7748543421b084d971b0b39a40d4b3518bdd7ef
korkeep <korkeep@khu.ac.kr>
2019-11-26 21:21:41 +0900

    Youtube API 검색기능 추가

---
+++ SW Online Lecture APP/app/src/main/java/com/example/swonlinelectureapp/PlayActivity.java
@@ -0,0 +1,47 @@
+package com.example.swonlinelectureapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class PlayActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private YouTubePlayerView ytpv;
+    private YouTubePlayer ytp;
+    final String serverKey="AIzaSyBdARQznrjtHIflil6qtPdPdjMy2MdFWTU";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        ytpv = (YouTubePlayerView) findViewById(R.id.youtubeplayer);
+        ytpv.initialize(serverKey, this);
+
+    }
+
+    @Override
+    public void onInitializationFailure(Provider arg0,
+                                        YouTubeInitializationResult arg1) {
+        Toast.makeText(this, "Initialization Fail", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onInitializationSuccess(Provider provider,
+                                        YouTubePlayer player, boolean wasrestored) {
+        ytp = player;
+        Intent gt =getIntent();
+        ytp.loadVideo(gt.getStringExtra("id"));
+    }
+
+}
--- SW Online Lecture APP/app/src/main/java/com/example/swonlinelectureapp/StartActivity.java
+++ SW Online Lecture APP/app/src/main/java/com/example/swonlinelectureapp/StartActivity.java
@@ -1,5 +1,20 @@
 package com.example.swonlinelectureapp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,7 +23,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class StartActivity extends AppCompatActivity {
+
+    //검색 기능 구현에 필요한 Parameter
+    static DrawableManager DM = new DrawableManager();
+    // String은 공백을 끝으로 인식하기 때문에 다른 대안 필요하다
+    String search_item;
+    AsyncTask<?, ?, ?> searchTask;
+    ArrayList<SearchData> sdata = new ArrayList<SearchData>();
+    final String serverKey="AIzaSyBdARQznrjtHIflil6qtPdPdjMy2MdFWTU";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +54,8 @@ public class StartActivity extends AppCompatActivity {
 
         //Home에서 넘겨받은 String
         Intent intent = getIntent();
-        String search_item = intent.getExtras().getString("search_item");
-
-        //테스트 부분(나중에 지워야)
-        TextView t1 = findViewById(R.id.result);
-        t1.setText(search_item);
+        search_item = intent.getExtras().getString("search_item");
+        searchTask = new searchTask().execute();
     }
 
     //상단 부분
@@ -45,10 +75,8 @@ public class StartActivity extends AppCompatActivity {
             //검색어 완료시
             @Override
             public boolean onQueryTextChange(String s) {
-                //테스트 부분(나중에 지워야)
-                TextView t1 = findViewById(R.id.result);
-                t1.setText(s);
-
+                search_item = s;
+                searchTask = new searchTask().execute();
                 return false;
             }
         });
@@ -66,5 +94,153 @@ public class StartActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //검색 기능 구현
+    private class searchTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                paringJsonData(getUtube());
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            ListView searchlist = (ListView) findViewById(R.id.searchlist);
+            StoreListAdapter mAdapter = new StoreListAdapter(
+                    StartActivity.this, R.layout.listview_start, sdata); //Json파싱해서 가져온 유튜브 데이터를 이용해서 리스트를 만들어줍니다.
+            searchlist.setAdapter(mAdapter);
+        }
+    }
+
+    public JSONObject getUtube() {
+        HttpGet httpGet = new HttpGet(
+                "https://www.googleapis.com/youtube/v3/search?"
+                        + "part=snippet&maxResults=20&q=" + search_item
+                        + "&key="+ serverKey); //검색 수행
+        // part(snippet),  q(검색값) , key(서버키)
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            response = client.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            InputStream stream = entity.getContent();
+            int b;
+            while ((b = stream.read()) != -1) {
+                stringBuilder.append((char) b);
+            }
+        } catch (ClientProtocolException e) {
+        } catch (IOException e) {
+        }
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject = new JSONObject(stringBuilder.toString());
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return jsonObject;
+    }
+
+    //파싱을 하면 여러가지 값을 얻을 수 있는데 필요한 값들을 세팅하셔서 사용하시면 됩니다.
+    private void paringJsonData(JSONObject jsonObject) throws JSONException {
+        sdata.clear();
+
+        JSONArray contacts = jsonObject.getJSONArray("items");
+
+        for (int i = 0; i < contacts.length(); i++) {
+            JSONObject c = contacts.getJSONObject(i);
+            String vodid = c.getJSONObject("id").getString("videoId");  //유튜브 동영상 아이디 값입니다. 재생시 필요합니다.
+
+            String title = c.getJSONObject("snippet").getString("title"); //유튜브 제목을 받아옵니다
+            String changString = "";
+            try {
+                changString = new String(title.getBytes("8859_1"), "utf-8"); //한글이 깨져서 인코딩 해주었습니다
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            String date = c.getJSONObject("snippet").getString("publishedAt") //등록날짜
+                    .substring(0, 10);
+            String imgUrl = c.getJSONObject("snippet").getJSONObject("thumbnails")
+                    .getJSONObject("default").getString("url");  //썸내일 이미지 URL값
+
+            sdata.add(new SearchData(vodid, changString, imgUrl, date));
+        }
+    }
+
+    String vodid = "";
+
+    public class StoreListAdapter extends ArrayAdapter<SearchData> {
+        private ArrayList<SearchData> items;
+        SearchData fInfo;
+
+        public StoreListAdapter(Context context, int textViewResourseId,
+                                ArrayList<SearchData> items) {
+            super(context, textViewResourseId, items);
+            this.items = items;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {// listview
+
+            // 출력
+            View v = convertView;
+            fInfo = items.get(position);
+
+            LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            v = vi.inflate(R.layout.listview_start, null);
+            ImageView img = (ImageView) v.findViewById(R.id.img);
+
+            String url = fInfo.getUrl();
+
+            String sUrl = "";
+            String eUrl = "";
+            sUrl = url.substring(0, url.lastIndexOf("/") + 1);
+            eUrl = url.substring(url.lastIndexOf("/") + 1, url.length());
+            try {
+                eUrl = URLEncoder.encode(eUrl, "EUC-KR").replace("+", "%20");
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            String new_url = sUrl + eUrl;
+
+            DM.fetchDrawableOnThread(new_url, img);  //비동기 이미지 로더
+
+            v.setTag(position);
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = (Integer) v.getTag();
+
+                    Intent intent = new Intent(StartActivity.this,
+                            PlayActivity.class);
+                    intent.putExtra("id", items.get(pos).getVideoId());
+                    startActivity(intent); //리스트 터치시 재생하는 엑티비티로 이동합니다. 동영상 아이디를 넘겨줍니다..
+                }
+            });
+
+            ((TextView) v.findViewById(R.id.title)).setText(fInfo.getTitle());
+            ((TextView) v.findViewById(R.id.date)).setText(fInfo
+                    .getPublishedAt());
+
+            return v;
+        }
     }
 }
